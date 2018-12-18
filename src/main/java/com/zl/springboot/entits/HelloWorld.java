b13cb630800f3202181bec44165d2e88c82bd26d
tangzhen3439 <45941700+tangzhen3439@users.noreply.github.com>
2018-12-18 18:22:45 +0800

    response to local
    response to local

--- src/main/java/com/zl/springboot/entits/HelloWorld.java
+++ src/main/java/com/zl/springboot/entits/HelloWorld.java
@@ -4,5 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("hello gitHub");
 		System.out.println("hello gitHub服务器");
+		System.out.println("服务器反馈本地");
+		
 	}
 }
