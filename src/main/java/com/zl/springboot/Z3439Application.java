34815855391bafb01e52aabc89b53e0e2557ed40
tangzhen3439 <tangzhen3439@126.com>
2018-12-18 18:02:45 +0800

    新建z3439项目

---
+++ src/main/java/com/zl/springboot/Z3439Application.java
@@ -0,0 +1,14 @@
+package com.zl.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Z3439Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Z3439Application.class, args);
+	}
+
+}
+
