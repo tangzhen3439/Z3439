b499ac99a2dfbfef463b94f87d9264625882b7ae
tangzhen3439 <tangzhen3439@126.com>
2018-12-18 18:13:41 +0800

    提交代码

--- src/main/java/com/zl/springboot/entits/HelloWorld.java
+++ src/main/java/com/zl/springboot/entits/HelloWorld.java
@@ -3,6 +3,6 @@ package com.zl.springboot.entits;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("hello gitHub");
+		System.out.println("hello gitHub服务器");
 	}
-
 }
